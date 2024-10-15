f37348fc5e3f99b5a65440ebd349eea57ecd3317
Laiba <laibamuskan5432@gmail.com>
2024-10-15 19:03:52 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const route = useRouter()
   return (
-    <div >
- <h1>This is Home page</h1>
- <Link href = "/navbar">Navbar</Link>
-     
- </div>
+    
+    <>
+    
+    <h1>This is Home page</h1>
+    <Link href={'/navbar'}>go to navbar</Link>
+    <button onClick={() =>route.push('/navbar')}>
+        
+      </button>
+    </>
   );
 }
